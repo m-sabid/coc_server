29b5182ef93b5a5f66b0b390b8caa66fad28b5e9
m-sabid <sabidofficial@gmail.com>
2023-11-18 18:06:13 +0600

    New

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.get("/api/clan/:tag", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
+    console.log(error)
   }
 });
 
